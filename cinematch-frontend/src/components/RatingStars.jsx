5c5ceb61459299fd37f9d23e8e3262190ca16f39
Rajveer Singh <panwarrajveer07@gmail.com>
2026-03-24 22:40:24 +0530

    phase one done

---
+++ cinematch-frontend/src/components/RatingStars.jsx
@@ -0,0 +1,7 @@
+export default function RatingStars() {
+  return (
+    <div>
+      RatingStars
+    </div>
+  );
+}
---
+++ cinematch-frontend/src/context/AuthContext.jsx
@@ -0,0 +1,11 @@
+import { createContext } from 'react';
+
+export const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+  return (
+    <AuthContext.Provider value={{}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
